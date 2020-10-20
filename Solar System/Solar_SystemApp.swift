6807d621680f75a7e33f53655efb8b85e09499f1
Vincent Spitale <43227465+vincentspitale@users.noreply.github.com>
2020-10-19 22:26:51 -0400

    Initial Commit

---
+++ Solar System/Solar_SystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  Solar_SystemApp.swift
+//  Solar System
+//
+//  Created by Vincent Spitale on 10/19/20.
+//
+
+import SwiftUI
+
+@main
+struct Solar_SystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
